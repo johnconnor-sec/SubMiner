a039c5465da3fdb20b4e980cd0ad8c3ba826e744
John <john.connor.sec7@gmail.com>
2025-04-07 21:40:41 -0400

    fix: get post by ID output

--- utils.py
+++ utils.py
@@ -4,3 +4,25 @@ from tabulate import tabulate
 def print_summary(results):
     table = [[r["title"], r["upvotes"], r["subreddit"], r["url"]] for r in results[:10]]
     print(tabulate(table, headers=["Title", "Upvotes", "Subreddit", "URL"]))
+
+
+def print_post_from_id(post):
+    markdown_output = f"""
+---
+{post["id"]}
+r/{post["subreddit"]}: {post["url"]}
+{post["created"]}
+{post["upvotes"]} upvotes
+---
+
+# {post["title"]}
+
+{post["text"]}
+
+--- 
+
+## COMMENTS: {post["comments"]}
+
+{post["comments_text"]}
+"""
+    print(markdown_output)
